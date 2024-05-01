7772072a447cca5db024de906f3e1a968f07aab1
Veereshbadiger <38921254+Veereshbadiger@users.noreply.github.com>
2024-05-02 05:14:03 +0530

    signin

---
+++ src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,12 @@
+import { View, Text,Image } from 'react-native'
+import React from 'react'
+import Logo from '../../../assets/images/suite.png';
+const SignInScreen = () => {
+  return (
+    <View>
+      <Image source={Logo}/>
+    </View>
+  )
+}
+
+export default SignInScreen
